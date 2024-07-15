19ac37bdab8431053b2f9803c7d5326392363c8d
Kirubahar S <kirubahars13@gmail.com>
2024-07-15 12:09:41 +0530

    created game mechanics

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Ball : MonoBehaviour
+{
+    public Rigidbody2D rb2d;
+    public float maxInitialAngle = 0.67f;
+    public float moveSpeed=4f;
+
+    private float startX = 0f;
+    public float maxStartY = 4f;
+
+    private void Start()
+    {
+        initialPush();
+    }
+
+    private void initialPush()
+    {
+        Vector2 dir = Random.value < 0.5f ? Vector2.left:Vector2.right;
+
+
+        dir.y = Random.Range(-maxInitialAngle, maxInitialAngle);
+        rb2d.velocity = dir * moveSpeed;
+    }
+
+    private void ResetBall()
+    {
+        float posY = Random.Range(-maxStartY, maxStartY);
+        Vector2 Position = new Vector2(startX, posY);
+        transform.position = Position;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        ScoreZone scoreZone = collision.GetComponent<ScoreZone>();
+        if (scoreZone)
+        {
+            ResetBall();
+            initialPush();  
+        }
+    }
+}
